a686c8efdf1934a08a35162c5db1dba145ec7d1c
Rehan Sakiwala <rehansakiwala@gmail.com>
2023-01-21 00:49:12 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly r= new RWOnly();
+        //r.name="Rehan";
+        //name has private access in com.driver.RWOnly
+        r.setName("Rehan");
+        System.out.println(r.getName());
+    }
 }
